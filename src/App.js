b58bb4571bddcf4006652f3fdca9e2975efb2c83
Tutao1995 <851542486@qq.com>
2019-08-09 09:32:59 +0800

    react demo01

--- src/App.js
+++ src/App.js
@@ -1,26 +1,187 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React,{Component} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class Square extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      value: null,
+    }
+  }
+  render(){
+    return (
+      <button 
+        className='square' 
+        onClick={() => this.props.onClick()}
+      >
+        {this.props.value}
+      </button>
+    )
+  }   
 }
 
-export default App;
+class Board extends Component {
+  renderSquare(i){
+    return <Square 
+      value={this.props.squares[i]}
+      onClick = {() => this.props.onClick(i)}
+      key={i}
+    />;
+  }
+  render(){
+    let liList = (length) => {
+      var res = [];
+      for (var i = 0; i < length; i++) {
+          res.push(this.renderSquare(i))
+      }
+      return res
+    }
+    return (
+      <div>
+        <div className="board-row">
+          {liList(9)}
+        </div>
+    
+      </div>
+    );
+  }
+}
+
+class Game extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      history:[
+        {squares:Array(9).fill(null)}
+      ],
+      xIsNext:true,
+      stepNumber:0,
+    }
+  }
+  handleClick(i){
+    const history = this.state.history.slice(0, this.state.stepNumber + 1);
+    const current = history[history.length-1];
+    const squares = current.squares.slice();
+    let liList = document.querySelectorAll('li');
+    liList.forEach(function(item,index){
+      let classList = item.classList;
+      if(classList.contains('active-li')){
+        classList.remove("active-li");
+      }
+    })
+    if (caculatorWinner(squares) || squares[i]) {  
+      return;
+    }
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+    this.setState({
+      history: history.concat([{
+        squares: squares,
+      }]),
+      xIsNext: !this.state.xIsNext,
+      stepNumber: history.length,
+    });
+  }
+  jumpTo(step){
+    let liList = document.querySelectorAll('li');
+    liList.forEach(function(item,index){
+      let classList = item.classList;
+      if(classList.contains('active-li')){
+        classList.remove("active-li");
+      }
+    })
+    document.querySelectorAll('li')[step].classList.add("active-li");
+    this.setState({
+      stepNumber: step,
+      xIsNext: (step % 2) === 0,
+    })
+  }
+  render() {
+    const history = this.state.history;
+    const current = history[this.state.stepNumber];
+    const winner = caculatorWinner(current.squares);
+    const moves = history.map((step, move) => {
+      let squares = step.squares.slice(),arr = [];
+      squares.forEach((item,index) => {
+        let temp = index + 1;
+        if(arr.length < 9){
+          if(item === 'X'){
+            arr.push({
+              "X":[Math.ceil(temp/3),((temp%3 === 0) ? 3 : temp%3)]
+            })
+          }
+          else if(item === 'O'){
+            arr.push({
+              "O":[Math.ceil(temp/3),((temp%3 === 0) ? 3 : temp%3)]
+            })
+          }
+        }
+      })
+
+     
+
+
+      const desc = move ? 
+        'Go to move #' + move :
+        'Go to start';
+      return (
+        <li key={move}>
+          <button
+            onClick={() => this.jumpTo(move)}
+          >
+            {desc}
+          </button>
+          <span>坐标系:  {JSON.stringify(arr)}</span>
+        </li>
+      )
+    })
+    let status;
+    if(winner){
+      status = 'winner:' + winner.symbol;
+    }
+    else{
+      status = "Next player: "+ (this.state.xIsNext ? 'X' : 'O');
+    }
+    return (
+      <div className="game">
+        <div className="game-board">
+          <Board 
+            squares={current.squares}
+            onClick = {(i) => {this.handleClick(i)}}
+          />
+        </div>
+        <div className="game-info">
+          <div>{status}</div>
+          <ol>{moves}</ol>
+        </div>
+      </div>
+    );
+  }
+}
+function caculatorWinner(squares){
+  let lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for(let i=0;i<lines.length;i++){
+    const [a,b,c] = lines[i];
+    if(squares[a] && squares[a] === squares[b] && squares[a] === squares[c]){
+      return {
+        symbol:squares[a],
+        point:lines[i]
+      }
+    }
+  }
+  return null
+}
+
+
+
+
+
+
+export default Game;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import Game from './App';
+import './app.css'
 
-ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+setInterval(() => {
+    ReactDOM.render(<Game date={new Date()}/>, document.getElementById('root'));
+},1000)
+
